ebecbf7b6ee4719a7538e36ec660230b0b059765
xChygyNx <Chykha110@gmail.com>
2019-09-16 16:30:42 +0300

    super optimization system

--- src/lem-in/tactical_moves.c
+++ src/lem-in/tactical_moves.c
@@ -6,17 +6,33 @@
 /*   By: pcredibl <pcredibl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/14 15:15:57 by astripeb          #+#    #+#             */
-/*   Updated: 2019/09/16 15:41:06 by pcredibl         ###   ########.fr       */
+/*   Updated: 2019/09/16 16:26:56 by pcredibl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
+
+static int	count_less_paths(t_listpath *list, t_listpath *path)
+{
+	int		path_c;
+	//t_path	*begin;
+
+	//begin = listpath->path;
+	path_c = 0;
+	while (path && path != list)
+	{
+		path_c++;
+		list = list->next;
+	}
+	return (path_c);
+}
 
 void		tactical_moves(t_lem *lem, t_ant *army, t_listpath *listpath)
 {
 	t_listpath	*cur_path;
 	t_ant		*soldier;
 	int			reserve;
+	int			less_c;
 
 	reserve = lem->ant_c;
 	soldier = army;
@@ -29,7 +45,8 @@ void		tactical_moves(t_lem *lem, t_ant *army, t_listpath *listpath)
 			soldier = soldier->next;
 			reserve--;
 			cur_path = cur_path->next;
-			if (!cur_path || cur_path->path_len > lem->ant_c)
+			less_c = count_less_paths(listpath, cur_path);
+			if (!cur_path || cur_path->path_len * less_c > lem->ant_c)
 				break ;
 		}
 	}
